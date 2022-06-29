af5da4d38db78c77d360e345043afba30d866b09
Bondok6 <kirolousbondok6@gmail.com>
2022-06-29 19:12:00 +0200

    Finish login, froget_password design pages

---
+++ layouts/auth.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="auth-page">
+    <div class="auth-page__image">
+      <img src="~/assets/images/login.PNG" alt="login-image" />
+    </div>
+
+    <div class="auth-page__form">
+      <img src="~/assets/images/logo.png" alt="logo" />
+      <h2>
+        مرحبا بكم في <br />
+        لوحة تحكم الكاشير
+      </h2>
+
+      <Nuxt />
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
---
+++ pages/auth/login.vue
@@ -0,0 +1,79 @@
+<template>
+  <el-form
+    :rules="loginFormRules"
+    :model="loginForm"
+    ref="loginForm"
+    class="form"
+    dir="rtl"
+  >
+    <h3 class="form__title">تسجيل الدخول</h3>
+    <p class="form__description">قم بأدخال البيانات للدخول</p>
+
+    <el-form-item prop="username" label=" ">
+      <el-input
+        v-model="loginForm.username"
+        placeholder="اسم المستخدم"
+        type="text"
+      />
+    </el-form-item>
+
+    <el-form-item prop="password" label=" ">
+      <el-input
+        v-model="loginForm.password"
+        placeholder="كلمة المرور"
+        type="password"
+        show-password
+      />
+    </el-form-item>
+
+    <nuxt-link to="/auth/forget_password">هل نسيت كلمه السر؟</nuxt-link>
+
+    <button type="submit" class="btn btn--pink" @click.prevent="login">
+      تسجيل الدخول
+    </button>
+  </el-form>
+</template>
+
+<script>
+export default {
+  layout: "auth",
+  data() {
+    return {
+      loginForm: {
+        username: "",
+        password: "",
+      },
+      loginFormRules: {
+        username: [{ required: true, message: "يجب ادخال اسم المستخدم" }],
+        password: [{ required: true, message: "يجب ادخال الرقم السري" }],
+      },
+    };
+  },
+  methods: {
+    login() {
+      this.$refs.loginForm.validate((valid) => {
+        if (valid) {
+          const loading = this.$loading({
+            lock: true,
+            text: "Loading",
+            spinner: "el-icon-loading",
+            background: "rgba(0, 0, 0, 0.7)",
+          });
+          console.log(this.loginForm);
+          try {
+            this.$message.success("تم تسجيل الدخول بنجاح");
+            // await this.$auth.loginWith("local", { data: this.loginForm });
+            // this.$router.push("/");
+          } catch (error) {
+            this.$message.error("خطأ في تسجيل الدخول");
+          } finally {
+            loading.close();
+          }
+        }
+      });
+    },
+  },
+};
+</script>
+
+<style></style>
